61ae5e2d81ee07a9f1ab1a937946d20d4aa72af3
andamtech <cradacus@gmail.com>
2011-10-21 16:24:45 +0200

    changed root to username to avoid unnecessary confusion

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?
-$conn = mysql_connect("localhost", "root", "password") or die(mysql_error());
+$conn = mysql_connect("localhost", "username", "password") or die(mysql_error());
 mysql_select_db('cradamCMS', $conn) or die(mysql_error());
 ?>
